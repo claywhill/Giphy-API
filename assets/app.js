fb34c92151f5246eafefd661945aa04e3c58a4a3
Clayton Hill <clayman0123@yahoo.com>
2020-03-10 16:57:04 -0600

    newbutton now appends gifs

--- assets/app.js
+++ assets/app.js
@@ -1,24 +1,24 @@
-var topics = ["greta van fleet", "kaleo", "tool", "five finger death punch", "mitch king", "the dead south", "the civil wars", "josh garrels"];
+var topics = ["greta van fleet", "kaleo", "tool", "metallica", "mitch king", "the dead south", "the civil wars", "josh garrels"];
 
 for (var i = 0; i < topics.length; i++) {
-   // $("#buttons").append("<button data-band = ' "+topics[i]+" ' class = 'newbutton'>" + topics[i] + "</button>");
+   $("#buttons").append("<button data-band = ' " + topics[i] + " ' class = 'newbutton'>" + topics[i] + "</button>");
 
     // template literals ``
 
-    $("#buttons").append(`
-    <button data-band ='${topics[i]}' class = 'newbutton'> ${topics[i]} </button>
-    `);
+    // $("#buttons").append(`
+    // <button data-band ='${topics[i]}' class = 'newbutton'> ${topics[i]} </button>
+    // `);
 
 }
 
-$(".submit").click(function() {
-    var input = $("input").val();
-    $("#buttons").append("<button data-band='"+input+"'class = 'newbutton'>" + input + "</button>");
-});
+$(".submit").on("click", function() {
+    var input = $("input").val().trim();
+    $("#buttons").append("<button data-band='"+ input +"'class = 'newbutton'>" + input + "</button>");
+    topics.push(input);
+})
 
-$(".newbutton").click(function() {
+$(document).on("click", ".newbutton", function() {
     var x = $(this).data("band");
-    console.log(x);
     var queryURL = "https://api.giphy.com/v1/gifs/search?q="+x+"&api_key=0Wzzyx4KXQ1EmBZXDMxaPeAiLYUtvhfC&limit=10";
     $.ajax({
         url : queryURL,
@@ -26,24 +26,41 @@ $(".newbutton").click(function() {
     }).then(function(response) {
         $("#bands").empty();
         for (var i = 0; i < response.data.length; i++) {
-            // var bandDiv = $("<div>");
-            // var p = $("<p>").text("Rating: " + response.data[i].rating);
-            // var bandImage = $("<img>");
-            // bandImage.attr("src",response.data[i].images.fixed_height.url);
-            // bandDiv.append(p);
-            // bandDiv.append(bandImage);
-
-
-
-            var bandDiv = $(`
-            <div>
-            <p>Rating: ${ response.data[i].rating}</p>
-            <img src="${response.data[i].images.fixed_height.url}">
-            </div>
-            `)
-            $("#bands").append(bandDiv);
+                var bandDiv = $("<div>");
+                var p = $("<p>").text("Rating: " + response.data[i].rating);
+                var bandImage = $("<img class = 'gif'>");
+                bandImage.attr("src", response.data[i].images.fixed_height.url);
+                bandDiv.append(bandImage);
+                $("#bands").append(bandDiv).append(p);
         }
+
+
+
+        //     var bandDiv = $(`
+        //     <div>
+        //     <p>Rating: ${ response.data[i].rating}</p>
+        //     <img class = "gif" src="${response.data[i].images.fixed_height.url}">
+        //     </div>
+        //     `)
+        //     $("#bands").append(bandDiv);
     })
+
+    // $(".gif").click(function() {
+    //     var state = $(this).attr("data-state");
+
+    //     if (state === "still") {
+    //         $(this).attr("src", $(this).attr("data-animate"));
+    //         $(this).attr("data-state", "animate");
+    //     }
+    //     if (state === "animate") {
+    //         $(this).attr("src", $(this).attr("data-still"));
+    //         $(this).attr("data-state", "still");
+    //     }
+    // })
+
+    
+
+
 })
 
 
